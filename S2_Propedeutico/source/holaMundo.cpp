bc06995ef934116d23ded29e6ac347117718ae32
rayggr <raymundo.gnz.rz@gmail.com>
2022-02-22 11:28:51 -0600

    primer commit

---
+++ S2_Propedeutico/source/holaMundo.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(){
+  std::cout << "Hola Mundo\n"<< std::endl;
+  return 0;
+}
---
+++ S2_Propedeutico/source/tiposDeDatos.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  cout << "Tamanio en bytes de <char>:" << sizeof(char) << endl;
+  cout << "Tamanio en bytes de <bool>:" << sizeof(bool) << endl;
+  cout << "Tamanio en bytes de <int>:" << sizeof(int) << endl;
+  cout << "Tamanio en bytes de <float>:" << sizeof(float) << endl;
+  cout << "Tamanio en bytes de <long>:" << sizeof(long) << endl;
+  cout << "Tamanio en bytes de <double>:" << sizeof(double) << endl;
+
+  return 0;
+}
