66eb9702e61b1c57766db7e5e4e8218630b1a1fa
rayggr <raymundo.gnz.rz@gmail.com>
2022-02-22 11:51:06 -0600

    arreglando funcion myFunc

--- S2_Propedeutico/source/tiposDeDatos.cpp
+++ S2_Propedeutico/source/tiposDeDatos.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 
+int myFunc(int a){
+  return 5+a;
+}
+
 int main(){
   cout << "Tamanio en bytes de <char>:" << sizeof(char) << endl;
   cout << "Tamanio en bytes de <bool>:" << sizeof(bool) << endl;
@@ -8,10 +12,6 @@ int main(){
   cout << "Tamanio en bytes de <float>:" << sizeof(float) << endl;
   cout << "Tamanio en bytes de <long>:" << sizeof(long) << endl;
   cout << "Tamanio en bytes de <double>:" << sizeof(double) << endl;
-
+  cout << myFunc(5) << endl;
   return 0;
-}
-
-int myFunc(){
-  return
 }
